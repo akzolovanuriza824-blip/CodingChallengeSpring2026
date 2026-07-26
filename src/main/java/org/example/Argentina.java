c440ac213f1294d8cbb902efcaefa5dab1615a69
nuriza221 <akzolovanuriza824@gmail.com>
2026-07-26 21:43:08 +0600

    Spain champ

--- src/main/java/org/example/Argentina.java
+++ src/main/java/org/example/Argentina.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Argentina {
     public static void main(String[] args) {
-        System.out.println("Argentina is not champions!!");
+        System.out.println("Spain is  champions!!");
     }
 }
