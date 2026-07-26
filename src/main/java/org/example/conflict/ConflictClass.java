9ed842e133a77954953557d47923c435a3c3672d
nuriza221 <akzolovanuriza824@gmail.com>
2026-07-26 21:23:12 +0600

    my conflict class

--- src/main/java/org/example/conflict/ConflictClass.java
+++ src/main/java/org/example/conflict/ConflictClass.java
@@ -2,7 +2,7 @@ package org.example.conflict;
 
 public class ConflictClass {
     public static void main(String[] args) {
-        System.out.println("Changes in master!!");
+        System.out.println("This is my conflict class!!");
 
     }
 }
